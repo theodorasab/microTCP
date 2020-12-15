77f87116f79892b8d0be7007dbffddcb659dc45d
EvaCh7 <73312284+EvaCh7@users.noreply.github.com>
2020-12-15 15:45:25 +0200

    Update test_microtcp_client.c

--- microTCP/test/test_microtcp_client.c
+++ microTCP/test/test_microtcp_client.c
@@ -34,12 +34,9 @@
 
 int main(int argc, char **argv)
 {
-    microtcp_header_t header1, header2, header3;
+    microtcp_header_t header1, header2;
     microtcp_sock_t socket_cl;
-    int sockfd;
-    char buffer[MAXLINE];
     struct sockaddr_in clientaddr;
-    void *hellocl = "Hello from client.";
 
     socket_cl = microtcp_socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -53,15 +50,12 @@ int main(int argc, char **argv)
 
     memset(&header1, 0, sizeof(header1));
     memset(&header2, 0, sizeof(header2));
-    memset(&header3, 0, sizeof(header3));
 
     header1.control = 1; header1.seq_number = 130; header1.ack_number = 456;
     header2.control = 4; header2.seq_number = 75; header2.ack_number = 112;
-    header3.control = 9; header3.seq_number = 13; header3.ack_number = 721;
 
     microtcp_send(&socket_cl, &header1, sizeof(header1), 0);
     microtcp_send(&socket_cl, &header2, sizeof(header2), 0);
-    microtcp_send(&socket_cl, &header3, sizeof(header3), 0);
 
     microtcp_shutdown(&socket_cl, 1);
 
@@ -69,25 +63,3 @@ int main(int argc, char **argv)
 
     return 0;
 }
-
-/*
-    if (bind(socket_cl->sd, (const struct sockaddr *)&clientaddr, sizeof(clientaddr)) < 0)
-    {
-        perror("Bind failed.\n");
-        exit(EXIT_FAILURE);
-    }
-*/
-
-/*
-     microtcp_header_t header;
-    // microtcp_header_t * head;
-   
-    memset(&header, 0, sizeof(header));
-    header.seq_number = 3;
-    //head=&header;
-    int sent = microtcp_send(&socket_cl, &header, sizeof(header), 0);
-    if (sent == -1)
-    {
-        printf("Error sending\n");
-    }
-    */
